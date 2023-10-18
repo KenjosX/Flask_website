f99a6391e5dfc87c1300308861ad2e5ff6328210
KenjosX <Kenzi.anteur@gmail.com>
2023-10-18 13:13:26 +0200

    testing git

--- website/views.py
+++ website/views.py
@@ -9,24 +9,6 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/notes', methods=['GET', 'POST'])
-@login_required
-def notes():
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash("Note is too short!", category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash("Note added!", category='success')
-            
-    user_notes = Note.query.filter_by(user_id=current_user.id).all()
-
-    return render_template("notes.html", user_notes=user_notes)
-
 @views.route('/play-guess-the-number', methods=['GET', 'POST'])
 def play_guess_the_number():
     if request.method == 'GET':
@@ -49,3 +31,22 @@ def play_guess_the_number():
             game_result = "Invalid input. Please enter a valid number."
 
         return render_template('home.html', game_result=game_result)
+
+@views.route('/notes', methods=['GET', 'POST'])
+@login_required
+def notes():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash("Note is too short!", category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category='success')
+            
+    user_notes = Note.query.filter_by(user_id=current_user.id).all()
+
+    return render_template("notes.html", user_notes=user_notes)
+
