4b6d4ac08ba4b5a76a76ebcb0a568a668840e3e7
KenjosX <Kenzi.anteur@gmail.com>
2023-11-02 14:36:16 +0100

    adding update and delete route

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 from .models import User, Note
 from . import db
@@ -50,3 +50,62 @@ def notes():
 
     return render_template("notes.html", user_notes=user_notes)
 
+
+@views.route('/post_note', methods=['GET', 'POST'])
+def post_note():
+    if request.method == 'POST':
+        # ancienne version du form
+        # note = request.form.get('note')
+        
+        # ça c'est une dictionnaire python
+        payload = request.get_json()
+
+        new_note = Note(data=payload["data"], user_id=payload["user_id"])
+        db.session.add(new_note)
+        db.session.commit()
+        flash("Note added!", category='success')
+
+    return (jsonify({"message": "Note added"}), 200)
+
+
+@views.route('/get_note/<int:user_id>/<int:note_id>', methods=['GET'])
+def get_note(user_id, note_id):
+
+    user = User.query.get(user_id)
+    note = Note.query.get(note_id)
+
+    if user is None or note is None or note.user_id != user.id:
+        return jsonify({"message": "User or note not found or not associated"}), 404
+
+    return jsonify({"id": note.id, "data": note.data, "date": note.date}), 200
+
+@views.route('/delete_note/<int:user_id>/<int:note_id>', methods=['DELETE'])
+def delete_note(user_id, note_id):
+
+    user = User.query.get(user_id)
+    note = Note.query.get(note_id)
+
+    if user is None or note is None or note.user_id != user.id:
+        return jsonify({"message": "User or note not found or not associated"}), 404
+
+    # Delete the note
+    db.session.delete(note)
+    db.session.commit()
+
+    return jsonify({"message": "Note deleted successfully"}), 200
+
+@views.route('/update_note/<int:user_id>/<int:note_id>', methods=['PUT'])
+def update_note(user_id, note_id):
+
+    user = User.query.get(user_id)
+    note = Note.query.get(note_id)
+
+    if user is None or note is None or note.user_id != user.id:
+        return jsonify({"message": "User or note not found or not associated"}), 404
+
+    # Update the note
+    payload = request.get_json()
+    note.data = payload["data"]
+    db.session.commit()
+
+    return jsonify({"message": "Note updated successfully"}), 200
